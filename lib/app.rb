007a4e449c70c1a10a7b8fe686258c934d57f5f6
anu <anu_iv2002@hotmail.com>
2019-02-18 17:06:22 +0000

    added if statement

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
 
-get '/cat' do
-  "<div style='border: 3px dashed red'>
-<img src='http://bit.ly/1eze8aE'>
-</div>"
+get '/random-cat' do
+
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat'do
+p params
+@name = params[:name]
+erb(:form)
 end
